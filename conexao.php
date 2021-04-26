9c49bf24a051994399f5d604d58abedcc6aa4a0b
joaopedrobordin <bpj7@hotmail.com>
2021-04-26 19:04:18 -0300

    PHP

--- conexao.php
+++ conexao.php
@@ -1,6 +1,22 @@
 <?php
+session_start();
+include("conexao.php")
 
+$nome = mysql_real_escape_string($conexao, trim($_POST["nome"]));          //  trim() TIRA OS ESPAÇOS DO FINAL E DO COMEÇO
+$usuario = mysql_real_escape_string($conexao, trim($_POST['usuario']));   
+$senha = mysql_real_escape_string($conexao, trim(md5($_POST['senha']))); //md5() CRIPTOGRAFA A SENHA 
 
+$sql = "select count(*) as total from usuario where usuario ='$usuario'";
+$result = mysqli_query($sql);
+$row = mysqli_fetch_assoc($result);
+
+if($row['total'] == 1) {
+    $_SESSION['usuario_existe'] = true;
+    header('location: cadastro.php');
+    exit
+}
+
+sql = "********"
 
 
 
